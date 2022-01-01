01c43f0fa3d4a84a7bf9e7d1a1951cf1c8df6b3c
AlexGatz <the.neocoder@gmail.com>
2022-01-01 08:34:20 -0500

    single request forwarding working

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@
 // To test:
 // 1.) start "nc -lvnp 8080"
 // 2.) cargo run
-// 3.) execute "curl -iv -X POST http://localhost:7878 -d '{"stuff":10, "otherstuff":50}'
+// 3.) execute "curl -iv http://localhost:7878 -d '{"stuff":10, "otherstuff":50}'
+// 4.) You can then edit request.txt and choose to forward to nc on port 8080. 
 
 // Current issue: Need to rework the loop. This started with the for loop to iterate through listener.incoming() but now that we are trying to add "forward" and "exit" functionality, we need to use a while loop of some kind.
 
@@ -24,24 +25,29 @@ use std::str;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let mut input = String::new();
 
-    // The refactor works initially, but I need to figure out how to get the loop to work with handle_client_requent.
+    println!("Waiting for inital connection/request from client...");
+
+    // Is talling incomming().next() needed here?
+    let stream = listener.incoming().next().unwrap().unwrap();
+    handle_client_request(stream);
+
     loop {
-        let stream = listener.incoming().next().unwrap().unwrap();
-
-        println!("entering handle_client_request");
-        handle_client_request(stream);
-        std::io::stdin().read_line(&mut input).unwrap();
+        let mut input = String::new();
         println!("Please type 'forward' or 'exit'.");
+        std::io::stdin().read_line(&mut input).unwrap();
 
         if input.trim() == "forward" {
             handle_server_request();
+            println!("\nRequest forwarded. Waiting for next request.\n");
+            let stream = listener.incoming().next().unwrap().unwrap();
+            handle_client_request(stream);
             continue;
         } else if input.trim() == "exit" {
             break;
         } else {
             println!("Invalid input");
+            continue;
         }
     }
 }
@@ -60,9 +66,10 @@ fn handle_client_request(mut stream: TcpStream) {
         buffer_str.trim_matches(char::from(0)),
     )
     .expect("Unable to write file");
+    println!("\nRequest written to request.txt\n");
 }
 
-// REFACTOR: This is just a placeholder for now.
+// REFACTOR: This is just a placeholder for now. The stream dies after the first request. The driver loop should maintain the stream in main().
 fn handle_server_request() {
     let mut file = std::fs::File::open("/home/neo/request.txt").unwrap();
     let mut contents = String::new();
